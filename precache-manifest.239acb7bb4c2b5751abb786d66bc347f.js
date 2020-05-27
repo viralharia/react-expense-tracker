80ce310df5a76829cf722e6012906268a4ecc204
Viral Haria <viralhar@gmail.com>
2020-05-27 18:01:49 +0530

    Updates

---
+++ precache-manifest.239acb7bb4c2b5751abb786d66bc347f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc663fc9f8897a9e898a175eab265496",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "ccd3cffe6089149774e5",
+    "url": "/react-expense-tracker/static/css/main.448ab8ca.chunk.css"
+  },
+  {
+    "revision": "9749c47e741cf35c5416",
+    "url": "/react-expense-tracker/static/js/2.632219c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.632219c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd3cffe6089149774e5",
+    "url": "/react-expense-tracker/static/js/main.8f2c826d.chunk.js"
+  },
+  {
+    "revision": "c407e71fe6588aded598",
+    "url": "/react-expense-tracker/static/js/runtime-main.907acc91.js"
+  }
+]);
